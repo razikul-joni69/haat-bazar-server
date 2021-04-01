4ff22da37a9910578b9219001537e55a8c1a2f68
Md Razikul Islam Joni <mdrazikulislamjoni@gmail.com>
2021-04-01 23:11:07 +0600

    upload delete and get single product work

---
+++ index.js
@@ -0,0 +1,64 @@
+
+const express = require("express");
+const app = express();
+const MongoClient = require("mongodb").MongoClient;
+const ObjectId = require("mongodb").ObjectID;
+const cors = require("cors");
+const bodyParser = require("body-parser");
+require("dotenv").config();
+
+const port = process.env.PORT || 5055;
+
+app.use(cors());
+app.use(bodyParser.json());
+
+app.get("/", (req, res) => {
+    res.send("Hello World!");
+});
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.v0ilw.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+});
+client.connect((err) => {
+    console.log("connection err", err);
+    const eventCollection = client.db("ph-assignment10").collection("products");
+
+    app.get("/products", (req, res) => {
+        console.log(eventCollection);
+        eventCollection.find().toArray((err, items) => {
+            res.send(items);
+        });
+    });
+
+    app.get("/product/:id", (req, res) => {
+        console.log('product-id', ObjectId(req.params.id));
+        eventCollection.find({_id: ObjectId(req.params.id)})
+        .toArray((err, items) => {
+          console.log('error', err, 'res', items);
+            res.send(items[0]);
+        });
+    });
+
+    app.post("/addEvent", (req, res) => {
+        const newEvent = req.body;
+        console.log("adding new event: ", newEvent);
+        eventCollection.insertOne(newEvent).then((result) => {
+            console.log("inserted count", result.insertedCount);
+            res.send(result.insertedCount > 0);
+        });
+    });
+
+    app.delete("/delete/:id", (req, res) => {
+        const id = ObjectId(req.params.id);
+        eventCollection
+            .findOneAndDelete({ _id: id })
+            .then((documents) => res.send(!!documents.value));
+    });
+
+});
+
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`);
+});
